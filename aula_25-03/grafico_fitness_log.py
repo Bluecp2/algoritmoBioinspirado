b566617ee841802a13151c4a79b657081045b926
Gabriel leite Silva <gabrieleitesilvax@gmail.com>
2026-04-06 17:19:22 -0300

    a

--- aula_25-03/grafico_fitness_log.py
+++ aula_25-03/grafico_fitness_log.py
@@ -1,11 +1,13 @@
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg') 
 import matplotlib.pyplot as plt
 
-# carregar dados
+
 df = pd.read_csv("dados.csv")
 
 # cria a plot
-plt.plot(df['Geracao'], df['Fitness'], marker='o')
+plt.plot(df['Geracao'], df['Fitness'], marker='o', linestyle='-', color='b')
 
 # cria a escala em log
 plt.yscale('log')
@@ -14,10 +16,13 @@ plt.yscale('log')
 plt.xlabel('Geração')
 plt.ylabel('Fitness (log)')
 
-#adiciona o titulo
-plt.title('Evolução do Fitness (Escala Logaritmica)')
+# adiciona o titulo
+plt.title('Evolução do Fitness (Escala Logarítmica)')
 
-#adiciona grid
-plt.grid(True)
+# adiciona grid
+plt.grid(True, which="both", ls="-", alpha=0.5)
 
-plt.show()
+
+plt.savefig('grafico_fitness_log.png', dpi=300)
+print("Sucesso: Gráfico salvo como 'grafico_fitness_log.png'")
+
